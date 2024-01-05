1f7a1aeb3f62ea5d19520283d7cbddc416a30b8c
ChillyBlaze <l135163642@gmail.com>
2024-01-05 16:17:42 +0800

    optimize gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,60 +1,45 @@
+val sdks: Pair<Int, Int> by rootProject.extra
+val versions: Pair<String, Int> by rootProject.extra
+val packageName: String by rootProject.extra
+
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.application)
+    alias(libs.plugins.kotlin)
 }
 
 android {
-    namespace = "com.chillyblaze.autonight"
-    compileSdk = 34
+    namespace = packageName
+    compileSdk = sdks.second
 
     defaultConfig {
-        applicationId = "com.chillyblaze.autonight"
-        minSdk = 30
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-        setProperty("archivesBaseName", "autonight-$versionName")
+        applicationId = packageName
+        minSdk = sdks.first
+        targetSdk = sdks.second
+        versionName = versions.first
+        versionCode = versions.second
     }
     buildTypes {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
         aidl = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        buildConfig = true
     }
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.material3:material3")
-    implementation("com.github.topjohnwu.libsu:core:5.2.2")
-    implementation("com.github.topjohnwu.libsu:service:5.2.2")
+    implementation(libs.ktx.core)
+    implementation(libs.ktx.runtime)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.activity)
+    implementation(libs.compose.viewmodel)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.material3)
+    implementation(libs.libsu.core)
+    implementation(libs.libsu.service)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,48 @@
+import com.android.build.gradle.api.AndroidBasePlugin
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.ByteArrayOutputStream
+
+
+fun String.execute() = runCatching {
+    ByteArrayOutputStream().also {
+        exec {
+            commandLine(this@execute.split(" "))
+            standardOutput = it
+        }
+    }.toString().trim().takeIf { it.isNotBlank() }
+}.getOrNull()
+
+val commitCount by lazy { "git rev-list --count HEAD".execute()?.toInt() }
+val recentTag by lazy { "git describe --abbrev=0 --tags".execute() }
+val sdks by extra { 30 to 34 }
+val versions by extra { (recentTag?.replace("v", "") ?: "1.0") to (commitCount ?: 0) }
+val packageName by extra("com.chillyblaze.autonight")
+
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    alias(libs.plugins.application) apply false
+    alias(libs.plugins.kotlin) apply false
+}
+
+subprojects {
+    plugins.withType<AndroidBasePlugin>().whenObjectAdded {
+        extensions.configure<BaseAppModuleExtension> {
+            composeOptions.kotlinCompilerExtensionVersion = libs.versions.kotlin.compiler.get()
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+            applicationVariants.all {
+                outputs.all {
+                    val filename = "${rootProject.name}_${versions.first}_${buildType.name}.apk"
+                    (this as BaseVariantOutputImpl).outputFileName = filename
+                }
+            }
+        }
+        tasks.withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = JvmTarget.JVM_17.target
+        }
+    }
 }
